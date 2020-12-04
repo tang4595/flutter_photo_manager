d83aa61cc884d0c403d2f3a463b2b245436c5c1d
tangyingli <tangyingli@kofo.io>
2020-12-04 10:33:29 +0800

    1、添加通过Resource获取文件大小方法；2、AssetEntity添加iOS专有的mediaSubtype类型。

--- example/ios/Runner/AppDelegate.m
+++ example/ios/Runner/AppDelegate.m
@@ -1,11 +1,20 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+#import "PMManager.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1), dispatch_get_main_queue(), ^{
+        
+        PMManager *manager = [PMManager new];
+        long long ret = [manager getAssetLength:@"4C6A2FE8-49FE-4B30-BFB2-85FAAF35E91E/L0/001"];
+        NSLog(@"ret: %lld", ret);
+    });
+    
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
